3b79d2e78e8c76b6eec57447fa73dff9632d6f55
poly000 <1348292515@qq.com>
2020-11-04 12:32:58 +0800

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use lazy_static::*;
-use std::fmt;
 use std::convert::From;
+use std::fmt;
 
 #[cfg(test)]
 mod tests {
-    use super::{BiliBv,BiliAv};
+    use super::{BiliAv, BiliBv};
 
     #[test]
     fn enc_works() {
@@ -20,81 +20,82 @@ mod tests {
 
     #[test]
     fn display_works() {
-        assert_eq!(format!("{}",BiliBv("1xx411c7XW".to_string())),
-                    "bv1xx411c7XW".to_string());
-        assert_eq!(format!("{}",BiliAv(170001)), "av170001".to_string())
+        assert_eq!(
+            format!("{}", BiliBv("1xx411c7XW".to_string())),
+            "bv1xx411c7XW".to_string()
+        );
+        assert_eq!(format!("{}", BiliAv(170001)), "av170001".to_string())
     }
 }
 
-const TABLE: &[u8;58] = b"fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
-const S: [u8;6] = [9,8,1,6,2,4];
+const TABLE: &[u8; 58] = b"fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
+const S: [u8; 6] = [9, 8, 1, 6, 2, 4];
 const XOR: u64 = 177451812;
 const ADD: u64 = 8728348608;
 
 lazy_static! {
-    static ref TR: [u8;256] = {
-        let mut tr = [0u8;256];
+    static ref TR: [u8; 256] = {
+        let mut tr = [0u8; 256];
         for i in 0..58 {
-            tr[ TABLE[i] as usize ] = i as u8;
+            tr[TABLE[i] as usize] = i as u8;
         }
         tr
     };
 }
 
-
-#[derive(Copy,Clone,Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct BiliAv(pub u64);
 
-#[derive(Clone,Debug)]
+#[derive(Clone, Debug)]
 pub struct BiliBv(pub String);
 
 impl BiliBv {
-
     /// Get BV Id ( NO 'bv' INCLUDED! ) :
-    /// 
+    ///
     /// ```
     /// use bv2av::*;
-    /// assert_eq!(BiliBv::from("17x411w7KC").get(), 
+    /// assert_eq!(BiliBv::from("17x411w7KC").get(),
     ///             "17x411w7KC");
     /// ```
-    /// 
+    ///
     pub fn get(&self) -> &str {
         &self.0
     }
 
     /// Decode Bv
-    /// 
+    ///
     /// ```
     /// use bv2av::*;
-    /// 
+    ///
     /// assert_eq!(BiliBv::from("1xx411c7XW").dec().get(), 314);
     /// ```
-    /// 
+    ///
     pub fn dec(&self) -> BiliAv {
         let str = self.get();
         BiliAv({
             let b = str.as_bytes();
             ((0..6)
-            .map(|i| {
-            let index = S[i as usize] as usize;
-            let index = b[index];
-            TR[ index as usize ] as u64 * 58u64.pow(i)
-            })
-            .sum::<u64>() - ADD) ^ XOR
+                .map(|i| {
+                    let index = S[i as usize] as usize;
+                    let index = b[index];
+                    TR[index as usize] as u64 * 58u64.pow(i)
+                })
+                .sum::<u64>()
+                - ADD)
+                ^ XOR
         })
     }
-
 }
 
 impl std::fmt::Display for BiliBv {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f,"bv{}", self.get())
+        write!(f, "bv{}", self.get())
     }
 }
 
 impl std::fmt::Display for BiliAv {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f,"av{}", self.get())
+        write!(f, "av{}", self.get())
     }
 }
 
@@ -110,31 +111,29 @@ impl From<&str> for BiliBv {
     }
 }
 
-
 impl BiliAv {
-
     /// Get AV Number:
-    /// 
+    ///
     /// ```
     /// use bv2av::BiliAv;
-    /// 
+    ///
     /// assert_eq!(BiliAv(170001).get()
-    ///     , 170001); 
-    /// 
+    ///     , 170001);
+    ///
     /// ```
-    
+
     pub fn get(&self) -> u64 {
         self.0
     }
 
     /// Encode Bv
-    /// 
+    ///
     /// ```
     /// use bv2av::{BiliAv,BiliBv};
-    /// 
+    ///
     /// assert_eq!(BiliAv(170001).enc().get(), "17x411w7KC");
     /// ```
-    
+
     pub fn enc(&self) -> BiliBv {
         let num = (self.0 ^ XOR) + ADD;
         BiliBv({
@@ -142,7 +141,7 @@ impl BiliAv {
             let b = unsafe { str.as_bytes_mut() };
             for i in 0..6 {
                 let index = S[i as usize] as usize;
-                b[index] = TABLE[ (num / 58u64.pow(i) % 58) as usize ];
+                b[index] = TABLE[(num / 58u64.pow(i) % 58) as usize];
             }
             str
         })
