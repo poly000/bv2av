6ff0c9af548a19dfc3218cbc6d304376143db0ae
poly000 <1348292515@qq.com>
2020-12-03 13:40:55 +0800

    rdit

--- src/lib.rs
+++ src/lib.rs
@@ -45,70 +45,12 @@ const TABLE: &[u8; 58] = b"fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKN
 const S: [u8; 6] = [9, 8, 1, 6, 2, 4];
 const XOR: u64 = 177451812;
 const ADD: u64 = 8728348608;
-
-const fn get_tr() -> [u8;256] {
-    let mut tr = [0u8; 256];
-    tr[TABLE[0] as usize] = 0;
-    tr[TABLE[1] as usize] = 1;
-    tr[TABLE[2] as usize] = 2;
-    tr[TABLE[3] as usize] = 3;
-    tr[TABLE[4] as usize] = 4;
-    tr[TABLE[5] as usize] = 5;
-    tr[TABLE[6] as usize] = 6;
-    tr[TABLE[7] as usize] = 7;
-    tr[TABLE[8] as usize] = 8;
-    tr[TABLE[9] as usize] = 9;
-    tr[TABLE[10] as usize] = 10;
-    tr[TABLE[11] as usize] = 11;
-    tr[TABLE[12] as usize] = 12;
-    tr[TABLE[13] as usize] = 13;
-    tr[TABLE[14] as usize] = 14;
-    tr[TABLE[15] as usize] = 15;
-    tr[TABLE[16] as usize] = 16;
-    tr[TABLE[17] as usize] = 17;
-    tr[TABLE[18] as usize] = 18;
-    tr[TABLE[19] as usize] = 19;
-    tr[TABLE[20] as usize] = 20;
-    tr[TABLE[21] as usize] = 21;
-    tr[TABLE[22] as usize] = 22;
-    tr[TABLE[23] as usize] = 23;
-    tr[TABLE[24] as usize] = 24;
-    tr[TABLE[25] as usize] = 25;
-    tr[TABLE[26] as usize] = 26;
-    tr[TABLE[27] as usize] = 27;
-    tr[TABLE[28] as usize] = 28;
-    tr[TABLE[29] as usize] = 29;
-    tr[TABLE[30] as usize] = 30;
-    tr[TABLE[31] as usize] = 31;
-    tr[TABLE[32] as usize] = 32;
-    tr[TABLE[33] as usize] = 33;
-    tr[TABLE[34] as usize] = 34;
-    tr[TABLE[35] as usize] = 35;
-    tr[TABLE[36] as usize] = 36;
-    tr[TABLE[37] as usize] = 37;
-    tr[TABLE[38] as usize] = 38;
-    tr[TABLE[39] as usize] = 39;
-    tr[TABLE[40] as usize] = 40;
-    tr[TABLE[41] as usize] = 41;
-    tr[TABLE[42] as usize] = 42;
-    tr[TABLE[43] as usize] = 43;
-    tr[TABLE[44] as usize] = 44;
-    tr[TABLE[45] as usize] = 45;
-    tr[TABLE[46] as usize] = 46;
-    tr[TABLE[47] as usize] = 47;
-    tr[TABLE[48] as usize] = 48;
-    tr[TABLE[49] as usize] = 49;
-    tr[TABLE[50] as usize] = 50;
-    tr[TABLE[51] as usize] = 51;
-    tr[TABLE[52] as usize] = 52;
-    tr[TABLE[53] as usize] = 53;
-    tr[TABLE[54] as usize] = 54;
-    tr[TABLE[55] as usize] = 55;
-    tr[TABLE[56] as usize] = 56;
-    tr[TABLE[57] as usize] = 57;
-    tr
-}
-const TR:[u8;256] = get_tr();
+const TR:[u8;256] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 13, 12, 46, 31, 43, 18, 40, 28, 5, 0, 0, 0, 0, 0, 0, 0, 54, 20, 15, 8, 39, 57, 45, 36, 0, 38, 51, 42, 49, 52, 0, 53, 7, 4, 9, 50, 10, 44,
+34, 6, 25, 1, 0, 0, 0, 0, 0, 0, 26, 29, 56, 3, 24, 0, 47, 27, 22, 41, 16, 0, 11, 37, 2, 35, 21, 17, 33, 30, 48, 23, 55, 32, 14, 19, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 #[derive(Copy, Clone, Debug)]
 pub struct BiliAv(pub u64);
 
